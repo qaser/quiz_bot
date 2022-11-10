38d2ddb4282cc9db3419de9df3c436d5949386ff
qaser <huji@mail.ru>
2022-11-10 18:06:46 +0500

    добавлены тексты приветственных надписей

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,14 @@
 TIME_ZONE = 'Asia/Yekaterinburg'
 # TIME_ZONE = 'Europe/Moscow'
+
+DEPARTMENTS = [
+    'КС-1,4',
+    'КС-2,3',
+    'КС-5,6',
+    'КС-7,8',
+    'КС-9,10',
+    'АиМО',
+    'ЛЭС',
+    'ЭВС',
+    'СЗК',
+]
