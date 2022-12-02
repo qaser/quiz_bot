1569b41b3abf32793e772cad33e2533b5f16969a
qaser <huji@mail.ru>
2022-12-02 12:15:17 +0500

    fix

--- utils/make_pdf.py
+++ utils/make_pdf.py
@@ -34,15 +34,15 @@ def report_department_pdf(year, quarter, department, results_set):
     regular_font = TrueTypeFont.true_type_font_from_file(
         font_path / 'Inter-Regular.ttf'
     )
-    thin_font = TrueTypeFont.true_type_font_from_file(
-        font_path / 'Inter-Thin.ttf'
-    )
+    # thin_font = TrueTypeFont.true_type_font_from_file(
+    #     font_path / 'Inter-Thin.ttf'
+    # )
     semibold_font = TrueTypeFont.true_type_font_from_file(
         font_path / 'Inter-SemiBold.ttf'
     )
-    italic_font = TrueTypeFont.true_type_font_from_file(
-        font_path / 'Inter-Italic.ttf'
-    )
+    # italic_font = TrueTypeFont.true_type_font_from_file(
+    #     font_path / 'Inter-Italic.ttf'
+    # )
 
     # заголовок страницы
     layout.add(
@@ -61,14 +61,14 @@ def report_department_pdf(year, quarter, department, results_set):
         number_of_rows=len(results_set)+1,
         number_of_columns=6,
         margin_top=Decimal(20),
-        column_widths=[
-            Decimal(0.7),
-            Decimal(3),
-            Decimal(2),
-            Decimal(1.7),
-            Decimal(2),
-            Decimal(1.7),
-        ],
+        # column_widths=[
+        #     Decimal(0.7),
+        #     Decimal(3),
+        #     Decimal(2),
+        #     Decimal(1.7),
+        #     Decimal(2),
+        #     Decimal(1.7),
+        # ],
     )
 
     for header in TABLE_HEADERS:
@@ -76,19 +76,20 @@ def report_department_pdf(year, quarter, department, results_set):
             TableCell(
                 Paragraph(
                     header,
-                    horizontal_alignment=Alignment.CENTERED,
-                    text_alignment=Alignment.CENTERED,
+                    # horizontal_alignment=Alignment.CENTERED,
+                    # text_alignment=Alignment.CENTERED,
                     font=semibold_font,
                 ),
             ),
         )
+
     for num, res in enumerate(results_set):
         table.add(
             TableCell(
                 Paragraph(
                     str(num+1),
-                    horizontal_alignment=Alignment.CENTERED,
-                    text_alignment=Alignment.CENTERED,
+                    # horizontal_alignment=Alignment.CENTERED,
+                    # text_alignment=Alignment.CENTERED,
                     font=regular_font,
                 ),
                 padding_top=Decimal(10),
@@ -98,8 +99,8 @@ def report_department_pdf(year, quarter, department, results_set):
             TableCell(
                 Paragraph(
                     res.get('user'),
-                    horizontal_alignment=Alignment.CENTERED,
-                    text_alignment=Alignment.CENTERED,
+                    # horizontal_alignment=Alignment.CENTERED,
+                    # text_alignment=Alignment.CENTERED,
                     font=regular_font,
                 ),
                 padding_top=Decimal(10),
@@ -109,8 +110,8 @@ def report_department_pdf(year, quarter, department, results_set):
             TableCell(
                 Paragraph(
                     res.get('date_input'),
-                    horizontal_alignment=Alignment.CENTERED,
-                    text_alignment=Alignment.CENTERED,
+                    # horizontal_alignment=Alignment.CENTERED,
+                    # text_alignment=Alignment.CENTERED,
                     font=regular_font,
                 ),
                 padding_top=Decimal(10),
@@ -120,8 +121,8 @@ def report_department_pdf(year, quarter, department, results_set):
             TableCell(
                 Paragraph(
                     str(res.get('grade_input')),
-                    horizontal_alignment=Alignment.CENTERED,
-                    text_alignment=Alignment.CENTERED,
+                    # horizontal_alignment=Alignment.CENTERED,
+                    # text_alignment=Alignment.CENTERED,
                     font=regular_font,
                 ),
                 padding_top=Decimal(10),
@@ -131,8 +132,8 @@ def report_department_pdf(year, quarter, department, results_set):
             TableCell(
                 Paragraph(
                     res.get('date_output'),
-                    horizontal_alignment=Alignment.CENTERED,
-                    text_alignment=Alignment.CENTERED,
+                    # horizontal_alignment=Alignment.CENTERED,
+                    # text_alignment=Alignment.CENTERED,
                     font=regular_font,
                 ),
                 padding_top=Decimal(10),
@@ -142,8 +143,8 @@ def report_department_pdf(year, quarter, department, results_set):
             TableCell(
                 Paragraph(
                     str(res.get('grade_output')),
-                    horizontal_alignment=Alignment.CENTERED,
-                    text_alignment=Alignment.CENTERED,
+                    # horizontal_alignment=Alignment.CENTERED,
+                    # text_alignment=Alignment.CENTERED,
                     font=regular_font,
                 ),
                 padding_top=Decimal(10),
