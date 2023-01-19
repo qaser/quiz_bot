eaef45aec01ebe119c6e824d4f777895afd15cc9
qaser <huji@mail.ru>
2023-01-19 23:09:04 +0500

    добавлена статистика

--- handlers/statistic.py
+++ handlers/statistic.py
@@ -1,17 +1,12 @@
-import datetime as dt
-import os
-
 from aiogram import Dispatcher, types
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.filters import Text
 from bson.objectid import ObjectId
 
 from config.bot_config import bot, dp
-from config.mongo_config import offers, users, key_rules, attentions, results, buffer
-from config.telegram_config import ADMIN_TELEGRAM_ID
-from utils.decorators import superuser_check, admin_check
+from config.mongo_config import buffer, results, users
+from utils.decorators import admin_check
 from utils.utils import calc_date, calc_test_type
+from aiogram.utils.exceptions import CantInitiateConversation
 
 
 @admin_check
@@ -37,20 +32,19 @@ async def stat_now(message: types.Message):
                 done.append(user.get('user_id'))
         if user_id not in done:
             undone.append(user)
-    data = buffer.insert_one({'users_list': undone})
-    if list(undone) == 0:
+    if len(undone) == 0:
         await message.answer(f'Текущие тесты прошли все пользователи')
     else:
+        data = buffer.insert_one({'users_list': undone})
         keyboard = types.InlineKeyboardMarkup()
         keyboard.add(
-            # types.InlineKeyboardButton(text='Не отправлять', callback_data='exit'),
             types.InlineKeyboardButton(text='Отправить', callback_data=f'buffer_{data.inserted_id}'),
         )
         users_list = ''
         for u in undone:
             users_list = f'{users_list}\n{u.get("full_name")}'
         await message.answer(
-            text=f'Текущие тесты не прошли:\n{users_list}\n\n Хотите им отправить уведомления?',
+            text=f'Текущие тесты не прошли:\n{users_list}\n\n Хотите отправить уведомления?',
             reply_markup=keyboard,
         )
 
@@ -59,8 +53,20 @@ async def stat_now(message: types.Message):
 async def send_notification(call: types.CallbackQuery):
     _, buffer_id = call.data.split('_')
     users_list = buffer.find_one({'_id': ObjectId(buffer_id)}).get('users_list')
-    await call.message.answer(users_list)
+    admin = call.message.from_user.full_name
+    for user in users_list:
+        try:
+            await bot.send_message(
+                chat_id = user.get('user_id'),
+                text=f'{admin} напоминает о необходимости прохождения теста',
+            )
+        except CantInitiateConversation:
+                await bot.send_message(
+                    chat_id=admin,
+                    text=f'Пользователь {user.get("full_name")} вероятно заблокировал бота.'
+                )
+                continue
 
 
 def register_handlers_statistic(dp: Dispatcher):
-    dp.register_message_handler(stat_now, commands='stat_now')
+    dp.register_message_handler(stat_now, commands='unsolve_test')
