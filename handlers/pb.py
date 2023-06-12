b4b2cda9ba2dc823db4d47479c14632583899f63
unknown <huji@mail.ru>
2023-06-12 16:09:19 +0500

    rpo

--- handlers/pb.py
+++ handlers/pb.py
@@ -10,6 +10,7 @@ from config.mongo_config import (pb_answers, pb_instruction_sections, pb_link,
                                  pb_questions, pb_users_stats, pb_programs, users, pb_rpo_program)
 from config.telegram_config import ADMIN_TELEGRAM_ID
 from scheduler.scheduler_func import send_quiz_button
+from utils.constants import TEST_TEXT
 from utils.utils import calc_grade, word_conjugate
 
 
@@ -17,6 +18,12 @@ def get_learning_question(count):
     q_id = pb_rpo_program.find_one({'count': count}).get('id_question')
     q_text = pb_questions.find_one({'p_id': q_id}).get('text')
     a_text = pb_answers.find_one({'id_questions': q_id, 'correct_answer': 1}).get('answer')
+    return f'Вопрос №{count} из 2243\n\n<b>{q_text}</b>\n\n{a_text}'
+
+
+def get_full_learning_question(id, count):
+    q_text = pb_questions.find_one({'p_id': id}).get('text')
+    a_text = list(pb_answers.find({'id_questions': id, 'correct_answer': 1}))
     return f'Вопрос №{count} из 2243\n\n<b>{q_text}</b>\n\n{a_text}'
 
 
@@ -61,9 +68,114 @@ async def learning_choice(call: types.CallbackQuery):
         )
 
 
+async def get_testing_questions(message: types.Message):
+    user_id = message.from_user.id
+    user_stats = pb_users_stats.find_one({'user_id': user_id})
+    if user_stats is None:
+        pb_users_stats.insert_one(
+            {'user_id': user_id, 'test_question_count': 0, 'test_result': []}
+        )
+    rand_questions_query = list(pb_rpo_program.aggregate([{ '$sample': { 'size': 20 } }]))
+    rand_questions_ids = [q.get('id_question') for q in rand_questions_query]
+    pb_users_stats.update_one(
+        {'user_id': user_id},
+        {'$set': {'test_quiz': rand_questions_ids, 'test_question_count': 0, 'test_result': []}}
+    )
+    await test_send_question(message)
+
+
+async def test_send_question(message: types.Message):
+    user_stats = pb_users_stats.find_one({'user_id': message.from_user.id})
+    count = user_stats.get('test_question_count')
+    if count == 19:
+        test_result = user_stats.get('test_result')
+        res = sum(1 for i in test_result if i == 1)
+        pb_users_stats.update_one(
+            {'user_id': message.from_user.id},
+            {'$set': {'test_result': [], 'test_question_count': 0}}
+        )
+        await message.edit_text(f'Тест завершён.\nПравильных ответов: {res} из 20-ти')
+    else:
+        keyboard = types.InlineKeyboardMarkup(row_width=5)
+        question_id = user_stats.get('test_quiz')[count]
+        question = pb_questions.find_one({'p_id': question_id})
+        answers_query = list(pb_answers.find({'id_questions': question_id}))
+        a_text = ''
+        buttons = []
+        for ans in answers_query:
+            sort_num = ans.get('sort_number')
+            text = ans.get('answer')
+            a_text = f'{a_text}\n<b>{sort_num}</b>. {text}'
+            btn = types.InlineKeyboardButton(text=sort_num, callback_data=f'answer_{ans.get("p_id")}_{question.get("p_id")}')
+            buttons.append(btn)
+        keyboard.row(*buttons)
+        q_text = question.get('text')
+        await message.edit_text(
+            f'<i>Вопрос №{count+1}\n</i><b>{q_text}</b>\n{a_text}',
+            reply_markup=keyboard,
+            parse_mode=types.ParseMode.HTML
+        )
+
+
+@dp.callback_query_handler(Text(startswith='answer_'))
+async def answer_check(call: types.CallbackQuery):
+    _, ans_id, q_id = call.data.split('_')
+    ans_check = pb_answers.find_one({'p_id': int(ans_id)}).get('correct_answer')
+    user_id = call.message.from_user.id
+    user_stats = pb_users_stats.find_one({'user_id': user_id})
+    test_result = user_stats.get('test_result')
+    test_result.append(ans_check)
+    test_count = user_stats.get('test_question_count') + 1
+    pb_users_stats.update_one(
+        {'user_id': user_id},
+        {'$set': {'test_result': test_result, 'test_question_count': test_count}}
+    )
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(
+        types.InlineKeyboardButton(text='Завершить', callback_data='testing_exit'),
+        types.InlineKeyboardButton(text='Следующий', callback_data='testing_next'),
+    )
+    if ans_check == 1:
+        text = 'Ответ верный'
+    else:
+        correct_ans = pb_answers.find_one({'id_questions': int(q_id), 'correct_answer': 1}).get('answer')
+        text = f'Ответ неверный\n\n<b>Правильный ответ:</b>\n{correct_ans}'
+    await call.message.edit_text(text, reply_markup=keyboard, parse_mode=types.ParseMode.HTML)
+
+
+@dp.callback_query_handler(Text(startswith='testing_'))
+async def answer_check(call: types.CallbackQuery):
+    _, choice = call.data.split('_')
+    user_id = call.message.from_user.id
+    if choice == 'exit':
+        pb_users_stats.update_one(
+            {'user_id': user_id},
+            {'$set': {'test_result': [], 'test_question_count': 0}}
+        )
+        await call.message.edit_text('Тест завершён, прогресс сброшен')
+    elif choice == 'next':
+        await test_send_question(call.message)
+
+
 async def testing(message: types.Message):
-    await message.answer('Режим "Самопроверка" в разработке. Выберите режим "Обучение"\n\n/rpo')
-    await message.delete()
+    # await message.answer('Режим "Самопроверка" в разработке. Выберите режим "Обучение"\n\n/rpo')
+    # await message.delete()
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(
+        types.InlineKeyboardButton(text='Отмена', callback_data='test_exit'),
+        types.InlineKeyboardButton(text='Начать', callback_data='test_start'),
+    )
+    await message.edit_text(TEST_TEXT, reply_markup=keyboard)
+
+
+@dp.callback_query_handler(Text(startswith='test_'))
+async def testing_choice(call: types.CallbackQuery):
+    _, choice = call.data.split('_')
+    if choice == 'exit':
+        call.message.delete()
+    elif choice == 'start':
+        # await start_testing(call.message)
+        await get_testing_questions(call.message)
 
 
 async def learning(message: types.Message):
