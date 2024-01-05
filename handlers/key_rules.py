46e0629114b94335f1f4ef18ceb6ec11228e7631
Alexey S <huji@mail.ru>
2024-01-05 23:41:25 +0500

    fix

--- handlers/key_rules.py
+++ handlers/key_rules.py
@@ -12,13 +12,15 @@ router = Router()
 @router.message(Command('key_rules'))
 async def send_key_rules(message: Message):
     photo_ids = [file_id.get('photo_id') for file_id in list(key_rules.find({}))]
-    # media_group = []
+    group_count = 0
     album_builder = MediaGroupBuilder(caption='Ключевые правила безопасности')
     for photo_id in photo_ids:
         # media_group.append(InputMediaPhoto(photo_id))
         album_builder.add_photo(photo_id)
-        if album_builder.__sizeof__ >= 6:
+        group_count += 1
+        if group_count >= 6:
             await bot.send_media_group(chat_id=message.chat.id, media=album_builder.build())
-    # if 0 < len(media_group) < 10:
-    #     await bot.send_media_group(chat_id=message.chat.id, media=media_group)
+            group_count = 0
+    if 0 < group_count < 10:
+        await bot.send_media_group(chat_id=message.chat.id, media=album_builder.build())
     await message.delete()
